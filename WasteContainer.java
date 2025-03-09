b70ca27d2fa3458d9f828faf8238ed075cbcd3f8
RoyalJonesGo <152839662+RoyalJonesGo@users.noreply.github.com>
2025-03-09 23:37:38 +0800

    Create WasteContainer.java

---
+++ WasteContainer.java
@@ -0,0 +1,21 @@
+public class WasteContainer {
+    private String type;
+    private int capacity;
+
+    public WasteContainer(String type, int capacity) {
+        this.type = type;
+        this.capacity = capacity;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public boolean isFull() {
+        return capacity >= 100;
+    }
+}
